9369da7d993f03812088a3419a1a19ce9923be0e
Veer Neti <Resume.qasdet@gmail.com>
2020-05-06 20:38:21 -0500

    cucumber.io sample

--- src/test/java/com/steps/MyStepdefs.java
+++ src/test/java/com/steps/MyStepdefs.java
@@ -1,7 +1,13 @@
 package com.steps;
+
+/*
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.en.When;*/
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class MyStepdefs {
     @When("^I search for \"([^\"]*)\"$")
